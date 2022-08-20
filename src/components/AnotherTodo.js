620cd184fde9dae2a1d340489b45c54847ba1160
Joel Perez <joel_perez65@yahoo.com>
2022-08-19 23:07:22 -0500

    added another todo component

---
+++ src/components/AnotherTodo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AnotherTodo = (props) => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <h2>Hello Again</h2>
+        </div>
+    );
+};
+
+export default AnotherTodo;
