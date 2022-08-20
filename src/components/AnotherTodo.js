f75070f1a60f87f6ad9dda11881b55bb9c467865
Joel Perez <joel_perez65@yahoo.com>
2022-08-19 23:11:10 -0500

    styled new component

--- src/components/AnotherTodo.js
+++ src/components/AnotherTodo.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import "./AnotherTodo.css";
 
 const AnotherTodo = (props) => {
     return (
-        <div>
+        <div className='hello'>
             <h1>Hello World</h1>
             <h2>Hello Again</h2>
         </div>
